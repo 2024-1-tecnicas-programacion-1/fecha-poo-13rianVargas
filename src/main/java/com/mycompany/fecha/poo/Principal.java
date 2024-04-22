af3da0b9e11051b370ddd8a6a4b80c52b1bcccf1
13rian Vargas <brianstevenvargasclavijo@gmail.com>
2024-04-22 00:08:02 -0500

    Se añade el código de Principal.

--- src/main/java/com/mycompany/fecha/poo/Principal.java
+++ src/main/java/com/mycompany/fecha/poo/Principal.java
@@ -1,5 +1,22 @@
 package com.mycompany.fecha.poo;
 
+import java.util.Scanner;
+
 public class Principal {
-    // TODO: Aquí va el código de la clase Principal
+    public static void main(String[] args) {
+        Scanner ent = new Scanner(System.in);
+        
+        System.out.println("Ingrese el día:");
+        int dia = ent.nextInt();
+        System.out.println("Ingrese el mes:");
+        int mes = ent.nextInt();
+        System.out.println("Ingrese el año:");
+        int ano = ent.nextInt();
+
+        Fecha fecha = new Fecha(dia, mes, ano);
+
+        if (fecha.validarFecha() == true){
+            System.out.println(fecha.fechaLarga());
+        }
+    }
 }
